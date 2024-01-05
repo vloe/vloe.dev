65fb99598fda188a5aaf68e748bfa0a6a6eada58
vloe <vloe.dev@gmail.com>
2024-01-06 00:14:38 +0100

    looking alright

---
+++ src/routes/+page.svelte
@@ -0,0 +1,39 @@
+<main class="mt-8">
+	<div class="flex flex-col space-y-6">
+		<article>
+			<div class="space-y-1.5">
+				<h2 class="font-mono text-lg tracking-tighter">info</h2>
+				<p class="text-sm text-muted-foreground">i'm an 20 y/o cs student at university of oslo.</p>
+			</div>
+		</article>
+		<article>
+			<div class="space-y-1.5">
+				<h2 class="font-mono text-lg tracking-tighter">projects</h2>
+				<ul class="space-y-4 py-2">
+					<li>
+						<a
+							href="https://exphil.io"
+							target="_blank"
+							rel="noreferrer"
+							class="flex flex-col space-y-1.5 text-sm"
+						>
+							<span class="font-medium underline underline-offset-4">exphil</span>
+							<span class="text-muted-foreground">a philosophy quiz app.</span>
+						</a>
+					</li>
+					<li>
+						<a
+							href="https://github.com/vloe/nvim"
+							target="_blank"
+							rel="noreferrer"
+							class="flex flex-col space-y-1.5 text-sm"
+						>
+							<span class="font-medium underline underline-offset-4">nvim</span>
+							<span class="text-muted-foreground">my nvim config.</span>
+						</a>
+					</li>
+				</ul>
+			</div>
+		</article>
+	</div>
+</main>
