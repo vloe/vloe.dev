8a520142983760e544b01b3befe2db7fac5561f9
vloe <vloe.dev@gmail.com>
2024-01-06 11:09:40 +0100

    fouc fix

--- src/lib/components/ThemeToggle.svelte
+++ src/lib/components/ThemeToggle.svelte
@@ -11,3 +11,21 @@
 	<Moon class="absolute h-4 w-4 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
 	<Sun class="h-4 w-4 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
 </Button>
+
+<!-- set theme preference (in head to avoid fouc) -->
+<svelte:head>
+	<script>
+		let themePreference;
+
+		if (typeof localStorage !== 'undefined' && localStorage.getItem('theme')) {
+			themePreference = localStorage.getItem('theme');
+		} else {
+			themePreference = window.matchMedia('(prefers-color-scheme: dark)').matches
+				? 'dark'
+				: 'light';
+		}
+
+		const isDark = themePreference === 'dark';
+		document.documentElement.classList[isDark ? 'add' : 'remove']('dark');
+	</script>
+</svelte:head>
