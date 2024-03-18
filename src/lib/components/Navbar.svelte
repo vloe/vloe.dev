57c9451e11a2ee6a55004212fdabb7746fc56448
vloe <vloe.dev@gmail.com>
2024-03-18 20:07:57 +0100

    remove x

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import X from "$lib/icons/X.svelte"
 	import Github from "$lib/icons/Github.svelte"
 	import Link from "$lib/components/Link.svelte"
 	import ThemeToggle from "$lib/components/ThemeToggle.svelte"
@@ -8,15 +7,6 @@
 <nav class="mt-12 flex items-center justify-between">
 	<a href="/" class="text-lg font-semibold"> vloe </a>
 	<div class="flex items-center space-x-2">
-		<Link
-			href="https://twitter.com/vloedev"
-			target="_blank"
-			rel="noreferrer"
-			variant="ghost"
-			size="xs"
-		>
-			<X class="h-4 w-4" />
-		</Link>
 		<Link
 			href="https://github.com/vloe"
 			target="_blank"
