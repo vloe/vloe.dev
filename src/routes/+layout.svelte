2eefc25f70beb82223ecf9a08243990917ede1b0
vloe <vloe.dev@gmail.com>
2024-05-21 09:05:27 +0200

    remove some stuff

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,9 @@
 <script>
 	import "../app.css"
-	import Navbar from "$lib/components/Navbar.svelte"
 
 	let { children } = $props()
 </script>
 
 <div class="container max-w-screen-sm px-8">
-	<Navbar />
 	{@render children()}
 </div>
