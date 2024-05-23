4e9078b604c3356e18ca8606021f134dc33c5d3a
vloe <vloe.dev@gmail.com>
2024-05-23 02:15:57 +0200

    header work

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,10 @@
+<div class="mt-16">
+	<header class="flex h-16 items-center justify-between">
+		<a href="/">
+			<div class="flex flex-col space-y-1">
+				<span class="text-lg font-bold">vloe</span>
+				<span class="text-sm">cs student</span>
+			</div>
+		</a>
+	</header>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,11 @@
 <script>
 	import "../app.css"
+    import Header from "$lib/components/Header.svelte"
 
 	let { children } = $props()
 </script>
 
-<div class="container max-w-screen-sm px-6">
+<div class="container max-w-screen-sm px-8">
+    <Header />
 	{@render children()}
 </div>
