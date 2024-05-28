c4f243ce04e2e7275b3747c336e689f5700cddf4
vloe <vloe.dev@gmail.com>
2024-05-28 04:35:27 +0200

    path fix?

--- svelte.config.js
+++ svelte.config.js
@@ -4,11 +4,10 @@ import { vitePreprocess } from "@sveltejs/vite-plugin-svelte"
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
-
 	kit: {
 		adapter: adapter({
 			paths: {
-				relative: false,
+				base: "/",
 			},
 		}),
 	},
