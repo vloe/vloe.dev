03fdb4b3ec427e7e794c31950f8f4c9939383768
vloe <vloe.dev@gmail.com>
2024-08-07 14:31:42 +0200

    title + desc

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,10 @@
-<p class="text-violet-400">test</p>
+<script lang="ts">
+	const title = "vloe"
+	const desc = "cs student"
+</script>
+
+<svelte:head>
+	<title>{title}</title>
+	<meta content={title} name="title" />
+	<meta content={desc} name="description" />
+</svelte:head>
