2bd53d486fe605fec9698a38280c77a7affd493d
vloe <vloe.dev@gmail.com>
2024-09-02 02:26:36 +0200

    few fixes

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 ## architecture
 
 a static site built with [sveltekit](https://kit.svelte.dev) and deployed to [cloudflare pages](https://pages.cloudflare.com)
-
-## credits
-
-design inspired by [shadcn](https://shadcn.com)
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -26,17 +26,7 @@
 	<main class="flex flex-col space-y-6">
 		<article class="flex flex-col space-y-3">
 			<h2 class="text-lg tracking-tight">about</h2>
-			<p>
-				hi, i'm a second-year cs undergrad at the university of oslo. looking for a swe
-				internship summer '25,
-				<a
-					href="https://x.com/messages/compose?recipient_id=1649091259762458626"
-					rel="noopener noreferrer"
-					target="_blank"
-				>
-					hmu
-				</a>(:
-			</p>
+			<p>hi, i'm a second-year cs undergrad at the university of oslo(:</p>
 		</article>
 		<article class="flex flex-col space-y-3">
 			<h2 class="text-lg tracking-tight">projects</h2>
@@ -45,24 +35,7 @@
 					<a href="https://shorter.dev" rel="noopener noreferrer" target="_blank">
 						shorter
 					</a>
-				</li>
-				<li>
-					<a
-						href="https://github.com/vloe/vloe.dev"
-						rel="noopener noreferrer"
-						target="_blank"
-					>
-						vloe.dev
-					</a>
-				</li>
-				<li>
-					<a
-						href="https://github.com/vloe/nvim"
-						rel="noopener noreferrer"
-						target="_blank"
-					>
-						nvim
-					</a>
+					(wip)
 				</li>
 			</ul>
 		</article>
