ab2a3603f82d11d8c0d9908b7ba6585ebf1a770d
vloe <vloe.dev@gmail.com>
2024-09-21 17:21:30 +0200

    small redesgin

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	const title = "vloe"
+	const title = "ɣloe"
 	const desc = "cs undergrad"
 </script>
 
@@ -12,27 +12,50 @@
 <div class="pt-24">
 	<header class="flex h-16 items-center justify-between">
 		<div class="flex flex-col justify-center gap-y-0.5">
-			<h1 class="text-lg font-semibold tracking-tight">ɣloe</h1>
-			<h3 class="text-sm">cs undergrad</h3>
+			<h1 class="text-lg font-semibold tracking-tight">{title}</h1>
+			<p class="text-sm">{desc}</p>
 		</div>
 		<div class="flex items-center gap-x-2">
-			<a href="https://x.com/vloedev" rel="noopener noreferrer" target="_blank"> twitter </a>
+			<a
+				class="text-pink-200 underline underline-offset-4"
+				href="https://x.com/vloedev"
+				rel="noopener noreferrer"
+				target="_blank"
+			>
+				twitter
+			</a>
 			/
-			<a href="https://github.com/vloe" rel="noopener noreferrer" target="_blank"> github </a>
+			<a
+				class="text-pink-200 underline underline-offset-4"
+				href="https://github.com/vloe"
+				rel="noopener noreferrer"
+				target="_blank"
+			>
+				github
+			</a>
 		</div>
 	</header>
 </div>
+
 <div class="pt-8">
 	<main class="flex flex-col space-y-6">
-		<article class="flex flex-col space-y-3">
+		<article class="flex flex-col gap-y-1.5">
 			<h2 class="text-lg tracking-tight">about</h2>
-			<p>hi, i'm a second-year cs undergrad at the university of oslo(:</p>
+			<p>
+				currently a second-year undergrad student (prosa @ uio). i love building stuff and
+				find ml interesting.
+			</p>
 		</article>
-		<article class="flex flex-col space-y-3">
+		<article class="flex flex-col gap-y-1.5">
 			<h2 class="text-lg tracking-tight">projects</h2>
-			<ul class="list-disc space-y-2 pl-[15px]">
+			<ul class="flex list-disc flex-col gap-y-1 pl-[15px]">
 				<li>
-					<a href="https://shorter.dev" rel="noopener noreferrer" target="_blank">
+					<a
+						class="text-pink-200 underline underline-offset-4"
+						href="https://shorter.dev"
+						rel="noopener noreferrer"
+						target="_blank"
+					>
 						shorter
 					</a>
 				</li>
