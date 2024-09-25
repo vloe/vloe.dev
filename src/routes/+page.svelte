bdcd8efcad5c41abd3672e35ef7921d38a6a3a02
vloe <vloe.dev@gmail.com>
2024-09-25 08:58:38 +0200

    some ui changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,7 +17,7 @@
 		</div>
 		<div class="flex items-center gap-x-2">
 			<a
-				class="text-pink-200 underline underline-offset-4"
+				class="text-pink-300 underline underline-offset-4"
 				href="https://x.com/vloedev"
 				rel="noopener noreferrer"
 				target="_blank"
@@ -26,7 +26,7 @@
 			</a>
 			/
 			<a
-				class="text-pink-200 underline underline-offset-4"
+				class="text-pink-300 underline underline-offset-4"
 				href="https://github.com/vloe"
 				rel="noopener noreferrer"
 				target="_blank"
@@ -40,18 +40,18 @@
 <div class="pt-8">
 	<main class="flex flex-col space-y-6">
 		<article class="flex flex-col gap-y-1.5">
-			<h2 class="text-lg tracking-tight">about</h2>
+			<h2 class="text-lg font-semibold tracking-tight">about</h2>
 			<p>
-				currently a second-year undergrad student (prosa @ uio). i love building stuff and
+				currently a second-year undergrad student (prosa@uio). i love building stuff and
 				find ml and hacking interesting.
 			</p>
 		</article>
 		<article class="flex flex-col gap-y-1.5">
-			<h2 class="text-lg tracking-tight">projects</h2>
+			<h2 class="text-lg font-semibold tracking-tight">projects</h2>
 			<ul class="flex list-disc flex-col gap-y-1 pl-[15px]">
 				<li>
 					<a
-						class="text-pink-200 underline underline-offset-4"
+						class="text-pink-300 underline underline-offset-4"
 						href="https://shorter.dev"
 						rel="noopener noreferrer"
 						target="_blank"
